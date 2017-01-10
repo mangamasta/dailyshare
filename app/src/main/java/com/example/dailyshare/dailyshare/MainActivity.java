bed38bea7f8787bc9ee1d66a3a06f1cbd588399f
dailyshare <du16493@gmail.com>
2017-01-10 17:52:01 +0000

    xml layout

--- app/src/main/java/com/example/dailyshare/dailyshare/MainActivity.java
+++ app/src/main/java/com/example/dailyshare/dailyshare/MainActivity.java
@@ -1,53 +1,34 @@
 package com.example.dailyshare.dailyshare;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button sign_up_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        sign_up_listener();
+    }
+
+    public void sign_up_listener(){
+        sign_up_button=(Button)findViewById(R.id.sign_up_button);
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent sign_up_activity = new Intent(getApplicationContext(), sign_up_Activity.class);
+                startActivity(sign_up_activity);
             }
         });
     }
 
     //This part is to test GitHub, Cheers
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/example/dailyshare/dailyshare/sign_up_Activity.java
@@ -0,0 +1,25 @@
+package com.example.dailyshare.dailyshare;
+
+/**
+ * Created by duvanh on 1/10/2017.
+ */
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class sign_up_Activity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
